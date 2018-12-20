1a76a2d6958b53b563aadf849504af1e22ce853a
linxixizhi <501963347@qq.com>
2018-12-20 09:43:23 +0800

    Added a test

--- manage.py
+++ manage.py
@@ -1,11 +1,12 @@
 import os
-from jobplus import create_app
+from jobplus import create_app, db
 from dotenv import load_dotenv
 
 dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
 if os.path.exists(dotenv_path):
     load_dotenv(dotenv_path)
-app = create_app('development')
+# app = create_app('development')
+app = create_app('testing')
 
 
 @app.cli.command()
