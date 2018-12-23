2d7d6994a4d153258830ddf5a8495ada5de4e0a4
linxixizhi <501963347@qq.com>
2018-12-23 11:56:36 +0800

    modified manage.py

--- manage.py
+++ manage.py
@@ -5,8 +5,8 @@ from dotenv import load_dotenv
 dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
 if os.path.exists(dotenv_path):
     load_dotenv(dotenv_path)
-# app = create_app('development')
-app = create_app('testing')
+app = create_app('development')
+# app = create_app('testing')
 migrate = Migrate(app, db)
 
 
